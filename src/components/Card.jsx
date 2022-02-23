b14cb5192506286764c63cfd4fd791ca2e44160c
nevenkaCuleva <culeva.n@yahoo.com>
2022-02-23 03:45:58 +0100

    Updating

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props){
         <div className="card--container">
             <div className="card--body">
                 <a href={props.link} target="_blank"> <img src={props.img} className="card--image"/></a>
-                <a href={props.code}><p className="card--button"> View Code </p></a>
+                <a href={props.code} target="_blank"> <p className="card--button"> View Code </p></a>
             </div>
 
         </div>
